9570e468cb4336a118ef19e967becd4668b4d327
Oluseyi-Abdull <149166369+Oluseyi-Abdull@users.noreply.github.com>
2023-10-27 17:51:07 +0100

    Update milestone_2.py
    Completed Tasks 3 and 4 with some  considerations on alternative approaches to the problems using try and except.

--- milestone_2.py
+++ milestone_2.py
@@ -1,4 +1,4 @@
-#Task_2 COMPLETED
+#Task_1 and Task_2 
 import random
 
 word_list = ["pineapple","mango","strawberry","watermelon","banana"]
@@ -16,3 +16,11 @@ word = choice(word_list)
 word
 # #In this step a method is created to grab a random fruit from the 
 # list of the fruitsthat are my favourite
+
+#Task_3 and 4
+guess = input("Please Enter a single letter guess: ")
+if len(guess) == 1:
+    print("Good guess!.")
+else:
+  #could use try except block and raise a value error instead. 
+    print("Oops! That is not a valud input.")
