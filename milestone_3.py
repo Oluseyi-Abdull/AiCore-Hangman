75d4461ecfa06d74eb63670dd59ffa99583083cb
Oluseyi-Abdull <149166369+Oluseyi-Abdull@users.noreply.github.com>
2023-10-27 19:54:09 +0100

    Update milestone_3.py
    Task 3 complete

--- milestone_3.py
+++ milestone_3.py
@@ -1,12 +1,15 @@
-## Loop to chek if the input from the user is an alphabetical character
-while True:
-  guess = input("Please Guess a letter: ")
-  if guess.isalpha() == True:
-    break
-  else:
-    print("Invalid letter. Please, enter a single alphabetical character.")
+def ask_for_input():
+  guess.lower()
+  ## Loop to chek if the input from the user is an alphabetical character
+  while True:
+    guess = input("Please Guess a letter: ")
+    if guess.isalpha() == True:
+      break
+    else:
+      print("Invalid letter. Please, enter a single alphabetical character.")
 
-if guess in word:
-  print(f"Good guess!{guess} is in the word")
-else:
-  print(f"Sorry, {guess} is not in the word. Try again!")
+def check_guess(guess):
+  if guess in word:
+    print(f"Good guess!{guess} is in the word")
+  else:
+    print(f"Sorry, {guess} is not in the word. Try again!")
