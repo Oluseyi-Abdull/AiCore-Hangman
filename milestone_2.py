e9ac5cede53ac7497be1109f0ab90283baf9fdc9
Oluseyi-Abdull <149166369+Oluseyi-Abdull@users.noreply.github.com>
2023-10-27 15:12:23 +0100

    Update milestone_2.py
    Added some error handling and random fruit acquisition using the random import.

--- milestone_2.py
+++ milestone_2.py
@@ -1,5 +1,18 @@
-#Task_1
-#Create a list of my 5 favourite fruits
+#Task_2 COMPLETED
+import random
+
 word_list = ["pineapple","mango","strawberry","watermelon","banana"]
 
-print(word_list)
+#Added try excepts to handle error when list is not entered into the function. 
+def choice(fruits):
+  if type(fruits) == list:
+    try:
+      print(random.choice(fruits)) # This is for testing purposes 
+    except:
+      raise TypeError("The values entered into the choice method must be of list Type")
+  return
+
+word = choice(word_list)
+word
+# #In this step a method is created to grab a random fruit from the 
+# list of the fruitsthat are my favourite
